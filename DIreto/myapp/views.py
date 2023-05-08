2e3ced777d9245abf1e46885db010dfb27779502
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-05-07 21:47:37 -0300

    Adicionado try except para conseguir mostrar home page

--- DIreto/myapp/views.py
+++ DIreto/myapp/views.py
@@ -8,13 +8,16 @@ from random import randint
 
 def index(request):
     id=randint(0, 100)
-    titulo=request.GET.get("titulo")
-    categoria=request.GET.get("categoria")
 
-    pub.pub(categoria, models.Postagem(id, titulo=titulo, conteudo=titulo,
+    try:
+        titulo=request.GET.get("titulo")
+        categoria=request.GET.get("categoria")
+
+        pub.pub(categoria, models.Postagem(id, titulo=titulo, conteudo=titulo,
                             usuario=models.Usuario.objects.get(email="Joao@email.com"),
                                data_do_evento="2021-05-06 22:59:00"))
-    
+    except:
+        pass
 
     posts = models.Postagem.objects.all()
     resposta = ""
